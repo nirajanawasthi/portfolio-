ea311cdb5738b1b47445a7ff1335b30baa843e34
NirajAwasthi1 <nirajanawasthi936@gmail.com>
2026-07-18 13:04:42 +0545

    Connected Neon PostgreSQL database

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -96,7 +96,13 @@ DATABASES = {
     )
 }
 
+import dj_database_url
 
+DATABASES = {
+    "default": dj_database_url.config(
+        default=os.environ.get("DATABASE_URL"), conn_max_age=600, ssl_require=True
+    )
+}
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
 
